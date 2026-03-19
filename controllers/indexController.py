b80ebd527a2de35e2c6f4360eb3f19ec6bbc8b39
Darwinpz <dpilaloazea@gmail.com>
2026-03-18 19:57:29 -0500

    Update indexController.py
    remove print

--- controllers/indexController.py
+++ controllers/indexController.py
@@ -1,5 +1,4 @@
 from flask import render_template, Blueprint
-from services.userService import UserService
 
 index_bp = Blueprint("index", __name__, url_prefix='/')
 
@@ -9,6 +8,4 @@ def indexRoute():
 
 @index_bp.route('/about', methods=['GET'])
 def aboutRoute():
-    users_data = UserService.get_all_users()
-    print(users_data["users"])
     return render_template('/views/about.html')
